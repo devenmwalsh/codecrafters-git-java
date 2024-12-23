0d3501746cc7caa43575c1b82fd4e568746a77c9
devenmwalsh <deven.walsh@du.edu>
2024-12-22 18:24:08 -0700

    Read Blob File

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
+import java.io.BufferedReader;
 
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     
      final String command = args[0];
     
@@ -26,6 +29,23 @@ public class Main {
            throw new RuntimeException(e);
          }
        }
+       
+       case "hash-object" -> {
+    	   String hash = args[1];
+    	   String hashDir = hash.substring(0, 2);
+    	   String hashFile = hash.substring(2);
+    	   
+    	   File blobFile = new File("./.git/objects/" + hashDir + "/" + hashFile);
+    	   
+    	   try {
+    	          String blob = new BufferedReader (new InputStreamReader(new InflaterInputStream(new FileInputStream(blobFile)))).readLine();
+    	          String content = blob.substring(blob.indexOf("\0")+1);
+    	          System.out.print(content);
+    	        } catch (IOException e) {
+    	          throw new RuntimeException(e);
+    	        }
+       }
+       
        default -> System.out.println("Unknown command: " + command);
      }
   }
